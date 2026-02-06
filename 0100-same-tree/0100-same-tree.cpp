3168243b3917cbdf908cbe7df5e27809cb49d41e
Lubhit khandelwal <165474929+Lubhitkhandelwal@users.noreply.github.com>
2026-02-06 17:48:38 +0530

    Time: 0 ms (100%), Space: 12.6 MB (95.8%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -15,12 +15,10 @@ public:
         if(p==NULL || q==NULL){
             return p==q;
         }
-        bool left = isSameTree(p->left,q->left);
-        bool right = isSameTree(p->right,q->right);    
 
-        if(left && right && p->val==q->val){
-                return true;
-        }
-        return false;
+        bool left = isSameTree(p->left,q->left);
+        bool right = isSameTree(p->right,q->right);
+
+        return left && right && p->val==q->val;
     }
 };
