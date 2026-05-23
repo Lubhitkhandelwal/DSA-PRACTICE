1832e77f5a25fb7f4c82e63b6765873016c67331
Lubhit khandelwal <165474929+Lubhitkhandelwal@users.noreply.github.com>
2026-05-23 08:59:45 +0530

    Time: 29 ms (41.81%), Space: 24.2 MB (18.79%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,55 +1,50 @@
 class Solution {
 public:
-    bool isValid(int i, int j, int m, int n){
-        if(i<0 || i>=m) return false;
-        if(j<0 || j>=n) return false;
-
-        return true;
+    bool isValid(int i,int j,int m,int n){
+        return(i>=0 && i<m && j>=0 && j<n);
     }
 
-    void markVis(vector<vector<char>>& grid, vector<vector<int>>& vis, int i, int j, int m, int n){
+    void bfs(vector<vector<int>>& vis, vector<vector<char>>& grid, int row, int col){
         queue<pair<int,int>> q;
-        q.push({i,j});
-        vis[i][j] = 1;
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        int dx[] = {-1,0,1,0};
+        int dy[] = {0,1,0,-1};
+
+        q.push({row,col});
+        vis[row][col] = 1;
 
         while(!q.empty()){
-            auto it = q.front();
+            auto [x,y] = q.front();
             q.pop();
 
-            int i = it.first;
-            for(int j=it.second-1;j<=it.second+1;j++){
-                if(isValid(i,j,m,n) && grid[i][j] == '1' && !vis[i][j]){
-                    vis[i][j] = 1;
-                    q.push({i,j});
-                }
-            }
-
-            int j = it.second;
-            for(int i=it.first-1;i<=it.first+1;i++){
-                if(isValid(i,j,m,n) && grid[i][j] == '1' && !vis[i][j]){
-                    vis[i][j] = 1;
-                    q.push({i,j});
+            for(int k=0;k<4;k++){
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+                if(isValid(nx,ny,m,n) && grid[nx][ny] == '1' && !vis[nx][ny]){
+                    vis[nx][ny] = 1;
+                    q.push({nx,ny});
                 }
             }
         }
-
     }
 
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> vis(m,vector<int>(n,0));
-        int cnt = 0;
+        vector<vector<int>> vis(m,vector<int> (n,0));
+        int ans=0;
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j] == '1' && !vis[i][j]){
-                    markVis(grid,vis,i,j,m,n);
-                    cnt++;
+                if(!vis[i][j] && grid[i][j]=='1'){
+                    ans++; 
+                    bfs(vis,grid,i,j);
                 }
             }
         }
-        return cnt;
+        return ans;
     }
 };
