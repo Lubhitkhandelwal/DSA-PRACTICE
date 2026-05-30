d21199e210844a54abd42f4a219cf500dcd1726e
Lubhit khandelwal <165474929+Lubhitkhandelwal@users.noreply.github.com>
2026-05-30 09:49:44 +0530

    Time: 0 ms (100%), Space: 11.4 MB (97.52%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        int arr[3] = {0};
-        for(int i=0;i<n;i++){
-            arr[nums[i]]++;
-        }      
+        // int n = nums.size();
+        // int arr[3] = {0};
+        // for(int i=0;i<n;i++){
+        //     arr[nums[i]]++;
+        // }      
 
-        int idx = 0;
-        for(int i=0;i<3;i++){
-            for(int j=0;j<arr[i];j++){
-                nums[idx] = i;
-                idx++;    
+        // int idx = 0;
+        // for(int i=0;i<3;i++){
+        //     for(int j=0;j<arr[i];j++){
+        //         nums[idx] = i;
+        //         idx++;    
+        //     }
+        // }
+
+        //Dutch National Flag Algorithm
+        int low = 0;
+        int mid = 0;
+        int high = nums.size()-1;
+
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                swap(nums[mid],nums[high]);
+                high--;
+                // mid++;
             }
         }
     }
