2c64a9abe6c14f37155aadd02dfd285a0c6c18a1
Lubhit khandelwal <165474929+Lubhitkhandelwal@users.noreply.github.com>
2026-06-03 18:11:43 +0530

    Time: 3 ms (37.46%), Space: 40.2 MB (58.87%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> leftP(n);
-        vector<int> rightP(n);
+        // int n = nums.size();
+        // vector<int> leftP(n);
+        // vector<int> rightP(n);
 
-        vector<int> ans;
-        leftP[0] = 1;
-        rightP[n-1] = 1;
-        for(int i=0;i<n-1;i++){
-            leftP[i+1] = nums[i]*leftP[i];
-            rightP[n-i-2] = nums[n-i-1]*rightP[n-i-1];
+        // vector<int> ans;
+        // leftP[0] = 1;
+        // rightP[n-1] = 1;
+        // for(int i=0;i<n-1;i++){
+        //     leftP[i+1] = nums[i]*leftP[i];
+        //     rightP[n-i-2] = nums[n-i-1]*rightP[n-i-1];
+        // }
+        // for(int i=0;i<n;i++){
+        //     ans.push_back(leftP[i]*rightP[i]);
+        // }
+        // return ans;
+
+
+        int n = nums.size();
+        vector<int> ans(n,1);
+
+        for(int i=1;i<n;i++){
+            ans[i] = ans[i-1]*nums[i-1];
         }
-        for(int i=0;i<n;i++){
-            ans.push_back(leftP[i]*rightP[i]);
+        int suff = 1;
+        for(int i=n-1;i>=0;i--){
+            ans[i] = ans[i]*suff;
+            suff *= nums[i]; 
         }
         return ans;
     }
