e1c5f67f346e30650ef5ef7d23c0582eb27e87c6
Lubhit khandelwal <165474929+Lubhitkhandelwal@users.noreply.github.com>
2026-06-05 00:36:44 +0530

    Time: 3 ms (66.59%), Space: 14.9 MB (37.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,27 +2,40 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
-        
         // Solution if use of Hashmap is not allowed
-        vector<pair<int,int>> temp;
+        // vector<pair<int,int>> temp;
+        // int n = nums.size();
+
+        // for(int i=0;i<n;i++){
+        //     temp.push_back({nums[i],i});
+        // }
+
+        // int sum = 0;
+        // sort(temp.begin(),temp.end());
+        // int i=0,j=n-1;
+        // while(i<j){
+        //     sum = temp[i].first+temp[j].first;
+        //     if(sum<target){
+        //         i++;
+        //     }else if(sum>target){
+        //         j--;
+        //     }else{
+        //         return {temp[i].second,temp[j].second};
+        //     }
+        // }
+        // return {-1,-1};
+
+        // If hashmap is allowed
         int n = nums.size();
-
+        unordered_map<int,int> m;
         for(int i=0;i<n;i++){
-            temp.push_back({nums[i],i});
-        }
+            int comp = target - nums[i];
 
-        int sum = 0;
-        sort(temp.begin(),temp.end());
-        int i=0,j=n-1;
-        while(i<j){
-            sum = temp[i].first+temp[j].first;
-            if(sum<target){
-                i++;
-            }else if(sum>target){
-                j--;
-            }else{
-                return {temp[i].second,temp[j].second};
+            if(m.find(comp)!=m.end()){
+                return {m[comp],i};
             }
+
+            m[nums[i]] = i;
         }
         return {-1,-1};
     }
