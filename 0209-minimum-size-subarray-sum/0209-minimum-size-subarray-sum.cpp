bc77151df3ba73f8d4643b4f52e701b2a1bb1b77
Lubhit khandelwal <165474929+Lubhitkhandelwal@users.noreply.github.com>
2026-07-10 14:52:14 +0530

    Time: 0 ms (100%), Space: 41.8 MB (39.68%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -4,20 +4,18 @@ public:
         int minLen = INT_MAX;
         int ans = 0;
         int n = nums.size();
+
         int s = 0;
-
         for(int e=0;e<n;e++){
-            ans += nums[e];
+            ans+=nums[e];
 
-            while(ans >= target){
+            while(ans>=target){
                 minLen = min(minLen,e-s+1);
-                ans -= nums[s];
+                ans-=nums[s];
                 s++;
             }
-        }
-        if(minLen==INT_MAX){
-            minLen = 0;
-        }
+        }    
+        if(minLen == INT_MAX) minLen = 0;
         return minLen;
     }
 };
